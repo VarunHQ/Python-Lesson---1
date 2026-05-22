d6dfe77ae8161ea02d708b98fac0d0ee071fc038
VarunHQ <varunsathish15@gmail.com>
2026-05-22 12:56:17 +0530

    Activity - 3 (Python)

---
+++ Activity - 3.py
@@ -0,0 +1,15 @@
+first_name = "Varun"
+last_name = "Sathish"
+full_name = first_name + last_name
+example = "Hello"*5
+
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Full Name:", full_name)
+print("String multiplied by 5: ", example)
+
+word = "Coding"
+print("Length of the word 'Coding':", len(word))
+print("FirstLetter of String:", word[0])
+print("Last Letter of String:", word[5])
+print("String sliced:", word[1:3])
