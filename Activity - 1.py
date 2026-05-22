8c648fbb8e0848fea05f5fe7bcff7c62deba42ec
VarunHQ <varunsathish15@gmail.com>
2026-05-22 12:36:19 +0530

    Activity - 1 (Python)

---
+++ Activity - 1.py
@@ -0,0 +1,12 @@
+age = 10 
+name = "Varun"
+weight = 20.1
+
+print(f"My name is {name} ")
+print(f"The type of weight variable is {type(weight)}")
+
+
+#Typecasting
+weight = int(weight)
+print(f"The type of weight variable is {type(weight)}")
+
