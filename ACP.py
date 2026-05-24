237394e5b393ccdd0ea7d1ddced0f7fb450712d0
VarunHQ <varunsathish15@gmail.com>
2026-05-24 10:57:10 +0530

    ACP (Python)

---
+++ ACP.py
@@ -0,0 +1,15 @@
+var1 = "Python"
+var2 = "Programming"
+full_var_name = var1 + var2
+example = "Coding"*10
+
+print("Variable 1:", var1)
+print("Variable 2:", var2)
+print("Full Variable Name:", full_var_name)
+print("String multiplied by 10: ", example)
+
+word = "VS Code"
+print("Length of the word 'VS Code':", len(word))
+print("FirstLetter of String:", word[0])
+print("Last Letter of String:", word[6])
+print("String sliced:", word[1:3])
